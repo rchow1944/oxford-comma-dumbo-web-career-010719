16440ef61d62173fbbeef3771cb9fed0117e5d3d
Raymond Chow <raychow1944@yahoo.com>
2018-12-20 01:53:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    end_str = array.pop
+    array.join(", ") << ", and #{end_str}"
+  end
 end
